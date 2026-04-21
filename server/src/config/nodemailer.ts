08755249d2af021a58d69a23b06d43a809400007
andy.mena <menaandy198@gmail.com>
2026-04-21 14:42:17 -0600

    feat: configure nodemailer for develop

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -7,9 +7,13 @@ const config = () => {
     return {
         host: process.env.SMTP_HOST,
         port: +process.env.SMTP_PORT,
+        secure: process.env.SMTP_PORT === '465', // true for 465, false for other ports,
         auth: {
           user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD
+          pass: process.env.SMTP_PASSWORD,
+        },
+        tls:{
+            rejectUnauthorized: false
         }
     }
 }
